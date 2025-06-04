cf0a09e9586f2fcbd30b8dc6b66566b5cb9e3b11
Michael Bestas <mkbestas@lineageos.org>
2025-06-04 04:35:37 +0300

    hawao: Add missing capsense_reset
    Change-Id: I73ac537ba6c19664e73943c19d5df09e6daa1884

--- hawao-vendor.mk
+++ hawao-vendor.mk
@@ -3293,6 +3293,7 @@ PRODUCT_PACKAGES += \
     libvidhance_horizon_correction \
     libvidhance_stabilizer \
     vendor.egistec.hardware.fingerprint@4.0 \
+    capsense_reset \
     android.hardware.biometrics.fingerprint@2.1-service-chipone \
     android.hardware.biometrics.fingerprint@2.1-service-ets \
     thermal-engine
